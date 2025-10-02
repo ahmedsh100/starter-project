6b2e3f07ada6eaa6568f0f3e8bf160aecdfdfec2
ahmedsh100 <ahmedmohamedsh100@gmail.com>
2025-10-03 01:23:55 +0300

    Display home page blogs

--- app/Http/Controllers/ThemeController.php
+++ app/Http/Controllers/ThemeController.php
@@ -2,13 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Blog;
 use Illuminate\Http\Request;
 
 class ThemeController extends Controller
 {
     public function index()
     {
-        return view('theme.index');
+
+        $blogs = Blog::paginate(1);
+
+        return view('theme.index',compact('blogs'));
     }
 
     public function category()
